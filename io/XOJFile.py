7e05721dc180264ed0e072b211f70126b9f753ba
natto <natto@natto-tp.(none)>
2011-06-23 14:25:44 -0400

    fix varnames

--- io/XOJFile.py
+++ io/XOJFile.py
@@ -2,10 +2,12 @@ import os, shutil, sys, time, re, gzip
 
 class XOJFile(object):
 
-    def __init__(self, xoj_filename, pdf_filename = None):
+    def __init__(self, xoj_filepath, pdf_filepath = None, width = None, height = None):
         self.pagecount = 0
-        self.pdf_filename = pdf_filename
-        self.xoj_filename = xoj_filename
+        self.pdf_filepath = pdf_filepath
+        self.xoj_filepath = xoj_filepath
+        self.width = width
+        self.height = height
 
     def doc_beg(self):
         self.ls_output = ["""<?xml version="1.0" standalone="no"?>
@@ -18,9 +20,9 @@ class XOJFile(object):
     def page_beg(self):
         self.pagecount += 1
         self.ls_output.extend([
-            '<page width="%s" height="%s">' % (width, height),
-            (self.pagecount is 1 and self.pdf_filename is not None \
-                 and '<background type="pdf" domain="absolute" filename="%s" pageno="%s" />' % (pdf_filename, self.pagecount)
+            '<page width="%s" height="%s">' % (self.width, self.height),
+            (self.pagecount is 1 and self.pdf_filepath is not None \
+                 and '<background type="pdf" domain="absolute" filename="%s" pageno="%s" />' % (self.pdf_filepath, self.pagecount)
                  or '<background type="pdf" pageno="%s" />' % (self.pagecount)),
             '<layer>'])
 
