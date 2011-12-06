0a9d15d5d8a1a18f08f77e95a343235257fd67fa
natto <natto@natto-tp.(none)>
2011-12-06 12:37:21 -0500

    dict to datarow class

---
+++ conversion/DictData.py
@@ -0,0 +1,20 @@
+
+class DataSet(object):
+    dc_header = {}
+    @classmethod
+    def headerrow(selfclass):
+        return sorted(selfclass.dc_header.keys())
+    def __init__(self, **kw):
+        for k, v in kw.items():
+            self[k] = v
+    def __setattr__(self, k, v):
+        self[k] = v
+    def __setitem__(self, k, v):
+        self.__dict__[k] = v
+        if k not in self.__class__.dc_header:
+            self.__class__.dc_header[k] = len(self.__class__.dc_header)
+    def __getitem__(self, k):
+        return self.__dict__[k]
+    def datarow(self):
+        return [self.__dict__.get(k, "NA") for k in self.__class__.headerrow()]
+  
