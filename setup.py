c24bb7ebf1de972e9350fc881bf69712e01f6da0
natto <natto@natto-tp.(none)>
2012-06-25 20:52:57 -0400

    add setup.py + move files to make setuptools setup.py work

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name="BeanBunny",
+        version = "0.0.1",
+        description = "assorted python files to interface with assorted file formats",
+        packages = ["BeanBunny", "BeanBunny.io", "BeanBunny.conversion"],
+        )
