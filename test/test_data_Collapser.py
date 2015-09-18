e959222cca9f62f2f10dbf364f816c6f4abcf37d
natto <natto@natto-tpy.none>
2015-09-18 13:16:52 -0400

    move Gen class to DSU

--- test/test_data_Collapser.py
+++ test/test_data_Collapser.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(0, '.')
 from BeanBunny.data import Collapser
+from BeanBunny.data import DataStructUtil as dsu
 import string
 import random
 
@@ -11,59 +12,8 @@ except:
 
 IS_CLI = __name__ == '__main__'
 
-class Gen:
-    '''
-    generate random nested data to test the collapser
-    
-    '''
-
-    def _random_generator(self):
-        if faker:
-            return faker.Faker().user_name
-        else:
-            return lambda: ''.join([random.choice(string.ascii_letters) for i in range(6)])
-
-    def __init__(self):
-        self.nresponse = 0
-        self.D = None
-        self.makesomething = self._random_generator()
-
-    def generate_child(self):
-        return dict(
-                response = self.makesomething(),
-                rt = random.randint(10,99),
-                )
-
-    def generate_parent(self, depth):
-        return {
-                'setting': self.makesomething()[:6],
-                'history': [],
-                }
-
-    def generate_nested(self, min_depth = 2, max_depth = 5):
-        if self.D:
-            return self.D
-
-        self.nresponse = 0
-        mydepth = random.randint(min_depth, max_depth)
-
-        def recur(remaining = 0):
-
-            if remaining is 0:
-                self.nresponse += 1
-                myd = self.generate_child()
-            else:
-                myd = self.generate_parent(mydepth-remaining)
-                for iresponse in range(random.randint(2,7)):
-                    myd['history'].append(recur(remaining - 1))
-            return myd
-
-        self.D = recur(mydepth)
-        return self.D
-
-
 def test_1pass_2pass_ideal_equality():
-    gen = Gen()
+    gen = dsu.DictGenerator()
     gen.generate_nested(3)
     print(gen.nresponse, "generated")
     # doing this will break the equivalence
